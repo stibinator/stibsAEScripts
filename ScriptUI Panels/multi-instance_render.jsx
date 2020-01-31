b7425f5b110976c3d93a960bc83bde809d9508bf
stibinator <stib@pureandapplied.com.au>
2020-01-31 13:41:18 +1100

    Update multi-instance_render.jsx

--- ScriptUI Panels/multi-instance_render.jsx
+++ ScriptUI Panels/multi-instance_render.jsx
@@ -3,6 +3,6 @@ var projectQueue = (app.project)?app.project.renderQueue:false;
 var externalQueue = [];
 for (var i= 1; i <= projectQueue.items.length; i++){
     var theItem = projectQueue.items[i];
-    externalQueue.push({"QNum": i, "itemName": theItem.comp.name, "settings": theItem.outputModule(1).getSettings( GetSettingsFormat.STRING )});
+    externalQueue.push({"QNum": i, "itemName": theItem.comp.name, "settings": theItem.outputModule(1).getSettings( GetSettingsFormat.json )});
 }
 alert(externalQueue[0].settings.toSource());
